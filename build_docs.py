03d496ba81648420b8000bd5f8ac550e8f0c7957
ilnurgi <ilnurgi@mail.ru>
2020-03-26 08:22:35 +0300

    update docs

--- build_docs.py
+++ build_docs.py
@@ -10,6 +10,8 @@ from io import StringIO
 from time import time
 from multiprocessing import Pool
 
+from htmlmin import minify
+
 from sphinx.cmd import build
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
